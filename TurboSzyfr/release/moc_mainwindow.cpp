15e3fad943c01efa81313094eaaf7a7339ec59f4
HootHoot1 <64024016+HootHoot1@users.noreply.github.com>
2020-10-26 21:42:52 +0100

    test

--- TurboSzyfr/release/moc_mainwindow.cpp
+++ TurboSzyfr/release/moc_mainwindow.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../Szyfrator/mainwindow.h"
+#include "../mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[4];
-    char stringdata0[70];
+    QByteArrayData data[10];
+    char stringdata0[152];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,11 +35,20 @@ static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 28), // "on_pushButtonwykonaj_clicked"
 QT_MOC_LITERAL(2, 40, 0), // ""
-QT_MOC_LITERAL(3, 41, 28) // "on_pushButtonwczytaj_clicked"
+QT_MOC_LITERAL(3, 41, 28), // "on_pushButtonwczytaj_clicked"
+QT_MOC_LITERAL(4, 70, 27), // "on_pushButtonzapisz_clicked"
+QT_MOC_LITERAL(5, 98, 35), // "on_spinBoxprzesuniecie_valueC..."
+QT_MOC_LITERAL(6, 134, 4), // "arg1"
+QT_MOC_LITERAL(7, 139, 5), // "cezar"
+QT_MOC_LITERAL(8, 145, 4), // "text"
+QT_MOC_LITERAL(9, 150, 1) // "s"
 
     },
     "MainWindow\0on_pushButtonwykonaj_clicked\0"
-    "\0on_pushButtonwczytaj_clicked"
+    "\0on_pushButtonwczytaj_clicked\0"
+    "on_pushButtonzapisz_clicked\0"
+    "on_spinBoxprzesuniecie_valueChanged\0"
+    "arg1\0cezar\0text\0s"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,7 +58,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -57,12 +66,18 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x08 /* Private */,
-       3,    0,   25,    2, 0x08 /* Private */,
+       1,    0,   39,    2, 0x08 /* Private */,
+       3,    0,   40,    2, 0x08 /* Private */,
+       4,    0,   41,    2, 0x08 /* Private */,
+       5,    1,   42,    2, 0x08 /* Private */,
+       7,    2,   45,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    6,
+    QMetaType::QString, QMetaType::QString, QMetaType::Int,    8,    9,
 
        0        // eod
 };
@@ -75,10 +90,13 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->on_pushButtonwykonaj_clicked(); break;
         case 1: _t->on_pushButtonwczytaj_clicked(); break;
+        case 2: _t->on_pushButtonzapisz_clicked(); break;
+        case 3: _t->on_spinBoxprzesuniecie_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 4: { QString _r = _t->cezar((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])));
+            if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = std::move(_r); }  break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
@@ -110,13 +128,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 5;
     }
     return _id;
 }
